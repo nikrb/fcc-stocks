46235e94ea3d61b85bb28fde1ca08e6eb6795735
nikrb <nik_rb@yahoo.com>
2017-09-08 12:45:51 +0100

    use weekday for date scale in Line

--- client/src/components/charts/Line.js
+++ client/src/components/charts/Line.js
@@ -6,7 +6,7 @@ export default class Line extends React.Component {
     const {xScale,yScale,data,stroke} = this.props;
     // define the line
     const valueline = d3.line()
-        .x(function(d) { return xScale(d.date); })
+        .x(function(d) { return xScale(d.weekday); })
         .y(function(d) { return yScale(d.close); });
     return (
         <path d={valueline(data)} fill="transparent" stroke={stroke}/>
