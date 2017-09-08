0ce26b4a852c94071c965802979461644aef4f4e
nikrb <nik_rb@yahoo.com>
2017-09-08 12:45:06 +0100

    add weekday scale to axis

--- client/src/components/charts/Axis.js
+++ client/src/components/charts/Axis.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as d3 from 'd3';
+import weekday from './weekday';
 
 export default class Axis extends React.Component {
   componentDidMount = () => {
@@ -10,7 +11,9 @@ export default class Axis extends React.Component {
   };
   renderAxis = () => {
     if( this.props.orient === "horiz"){
-      const haxis = d3.axisBottom( this.props.scale);
+      const dateFormat = d3.timeFormat( "%b-%d");
+      const haxis = d3.axisBottom( this.props.scale)
+        .tickFormat( (d) => dateFormat( weekday.invert(d)));
       // FIXME: this.refs.axis isn't working here
       haxis( d3.select( '.horiz'));
     } else {
