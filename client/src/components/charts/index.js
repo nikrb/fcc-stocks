7ea679ab0dcdd0f79e43a9046803a7678924cfd3
nikrb <nik_rb@yahoo.com>
2017-09-08 12:42:35 +0100

    update charts index

--- client/src/components/charts/index.js
+++ client/src/components/charts/index.js
@@ -1,4 +1,6 @@
 import LineChart from './LineChart';
 import Legend from './Legend';
+import weekday from './weekday';
+import {genLastWeekdays, normaliseDates} from './util';
 
-export {LineChart,Legend};
+export {LineChart,Legend, genLastWeekdays, normaliseDates, weekday};
